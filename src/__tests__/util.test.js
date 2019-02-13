9e4fb4413d751cdc389e7ead6fe55bcca143d2b1
Angus Grieve-Smith <grievesa@newschool.edu>
2019-02-13 18:36:56 -0500

    use exportFunctions to access functions called by other functions within the same module

--- src/__tests__/util.test.js
+++ src/__tests__/util.test.js
@@ -1,5 +1,4 @@
-//const util = require("../util");
-import {prefixUtil, doublePrefixUtil} from "../util";
+import util from "../util";
 
 // jest.mock("../util");
 
@@ -7,17 +6,23 @@ describe("prefixUtil() tests", function () {
     test("prefixUtil() prefixes", () =>{
         const testInput = "foo";
         const targetOutput = "prefixfoo";
-        const testOutput = prefixUtil(testInput);
+        const testOutput = util.prefixUtil(testInput);
 
         expect(testOutput).toEqual(targetOutput);
     });
 });
 
 describe("doublePrefixUtil() tests", function () {
+
+    let mockPrefixUtil = jest.fn();
+    
     test("doublePrefixUtil() prefixes twice", () =>{
         const testInput = "foo";
-        const targetOutput = "prefixprefixfoo";
-        const testOutput = doublePrefixUtil(testInput);
+        const targetOutput = "mockPrefixOutput";
+
+        util.prefixUtil = jest.fn().mockReturnValue(targetOutput);
+        
+        const testOutput = util.doublePrefixUtil(testInput);
 
         expect(testOutput).toEqual(targetOutput);
     });
