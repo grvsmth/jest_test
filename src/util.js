6708bd9007399da8783a0a30dabcade64697b284
Angus Grieve-Smith <grievesa@newschool.edu>
2019-02-13 17:42:57 -0500

    Added util.test.js

--- src/util.js
+++ src/util.js
@@ -1,3 +1,11 @@
 export function prefixUtil(inputString) {
     return "prefix" + inputString;
 }
+
+export function doublePrefixUtil(inputString) {
+    let outputString = inputString;
+    for (let i = 0; i < 2; i++) {
+        outputString = prefixUtil(outputString);
+    }
+    return outputString;
+}
